2097f9c1300613edbe0add71be5a2f089816a322
Mohammedkash <99675901+Mohammedkash@users.noreply.github.com>
2022-03-08 10:24:37 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("add-btn").addEventListener("click", function () {
+    let myTxt = document.getElementById("txt").value
+    document.getElementById("result").innerHTML = document.getElementById("result").innerHTML + myTxt
+    //----------------------------------------------------------------
+    //save the old document =  concatenate the old document with myTxt
+    //document.body.innerHTML = document.body.innerHTML+ myTxt ;
+
+
+
+
+}
+
+)
